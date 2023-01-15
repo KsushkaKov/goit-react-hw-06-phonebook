d3b836ae941ad1542dcf62cc85280e51f5cff2f2
Ksenia Kovalenko <ksushka.ua@gmail.com>
2023-01-15 13:57:43 +0200

    edited app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,9 @@
 import React from 'react';
-// import { useState, useEffect } from 'react';
-// import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { ContactFilter } from './ContactFilter/ContactFilter';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(localStorage.getItem('contactList')) ?? []
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contactList', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const addContact = (name, number) => {
-  //   const contactInList = contacts.some(
-  //     contact => contact.name.toLowerCase() === name.toLowerCase()
-  //   );
-  //   if (contactInList) {
-  //     alert(`⚠ Oops... Contact ${name} already in list!`);
-  //     return;
-  //   }
-  //   const newContact = {
-  //     name,
-  //     number,
-  //     id: nanoid(),
-  //   };
-  //   setContacts([...contacts, newContact]);
-  // };
-
-  // const handleDelete = id => {
-  //   setContacts(contacts.filter(newContact => newContact.id !== id));
-  // };
-
-  // const onInputFilter = () => {
-  //   return contacts.filter(newContact =>
-  //     newContact.name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-  // };
-
-  // const handleChange = e => {
-  //   const { value } = e.target;
-  //   setFilter(value);
-  // };
-
   return (
     <div
       style={{
@@ -61,7 +19,7 @@ export const App = () => {
       }}
     >
       <h1>Phonebook ☎</h1>
-      <ContactForm /*addContact={addContact}*/ />
+      <ContactForm />
       <h2>Contacts 📑</h2>
       <ContactFilter />
       <ContactList />
